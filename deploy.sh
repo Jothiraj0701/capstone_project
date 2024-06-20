620466d6772854724fc6f0e8c24dcad9011d1797
pavithra.p@xenovex.com <pavithra.p@xenovex.com>
2024-06-20 18:18:35 +0530

    jenkins File Modified

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-
-# Ensure necessary environment variables are set
-if [ -z "$KEYFILE" ] || [ -z "$SSH_USER" ] || [ -z "$EC2_INSTANCE_IP" ]; then
-    echo "Missing necessary environment variables (KEYFILE, SSH_USER, EC2_INSTANCE_IP)"
-    exit 1
-fi
-
 # SSH into your EC2 instance and run the deployment commands
-ssh -o StrictHostKeyChecking=no -i "$KEYFILE" "$SSH_USER@$EC2_INSTANCE_IP" << EOF
+ssh -i C:\Users\jothiraj\Downloads\testvm.pem ubuntu@3.109.49.240 << EOF
     cd /home/ubuntu/capstone_project
     docker-compose down
     docker-compose pull
